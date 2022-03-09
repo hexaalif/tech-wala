c9c746cffd382640aa0f2ae995490ecb5c138eb9
hexaalif <hexaalif2020@gamil.com>
2022-03-09 12:05:42 +0600

    responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('okay babu')
+const searchPhone = () => {
+    const searchInput = document.getElementById('search-input');
+    const searchText = searchInput.value;
+    // console.log(searchText);
+    searchInput.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(api => displayPhoneResult(api.data))
+}
+
+const displayPhoneResult = data => {
+    
+}
