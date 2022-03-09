2c73738f54ffdb3a7f3c266a99cfd7c9ac17cdb9
hexaalif <hexaalif2020@gamil.com>
2022-03-09 12:41:19 +0600

    search-result found

--- js/script.js
+++ js/script.js
@@ -11,5 +11,23 @@ const searchPhone = () => {
 }
 
 const displayPhoneResult = data => {
-    
+    const searchResult = document.getElementById('search-result');
+    data.forEach(datas => {
+        console.log(datas);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML= `
+        <div class="card text-center rounded-3">
+            <img src="${datas.image}" class="card-img-top w-50 h-75 mx-auto pt-3" alt="...">
+            <div class="card-body">
+                <h3 class="card-title">${datas.phone_name}</h3>
+                <h4>Brand: ${datas.brand}</h4>
+                <button onclick="" class="bg-primary text-white px-3 py-2 border-0 rounded-pill fw-bold">Explore now</button>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div)
+    })
 }
+
+const exploreNow = mealId
