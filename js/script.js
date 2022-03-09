4e60866bdad9c051d0e9a70bf2180f441d69823e
hexaalif <hexaalif2020@gamil.com>
2022-03-09 19:30:35 +0600

    last commit

--- js/script.js
+++ js/script.js
@@ -1,78 +1,84 @@
 // error message block
-document.getElementById('alert').style.display = 'none';
-document.getElementById('alert2').style.display = 'none';
+document.getElementById("alert2").style.display = "none";
 // input value clear & get input
 const searchPhone = () => {
-    const searchInput = document.getElementById('search-input');
-    const searchText = searchInput.value;
-    searchInput.value = '';
-    
-    if(searchText === ""){
-        document.getElementById('alert2').style.display = 'block';
-    }
-    else{
-        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-        fetch(url)
-        .then(res => res.json())
-        .then(api => displayPhoneResult(api.data))
+  const searchInput = document.getElementById("search-input");
+  const searchText = searchInput.value;
+  searchInput.value = "";
+  document.getElementById("alert2").style.display = "none";
+  if (searchInput == "") {
+    searchResult.innerHTML = "";
+    document.getElementById("phone-details").innerHTML = "";
+    document.getElementById("alert2").style.display = "block";
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((api) => displayPhoneResult(api.data))
 
-        .catch((error) => displayError(error))
-    }
-}
+      .catch((error) => displayError(error));
+    document.getElementById("phone-details").innerHTML = "";
+  }
+};
 
 // error handle
-const displayError = error => {
-    document.getElementById('alert2').style.display = 'block';
-}
+const displayError = (error) => {
+  document.getElementById("alert2").style.display = "block";
+};
 
 // Search Data show
-const displayPhoneResult = data => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.innerHTML = '';
-    if(searchResult == ''){
-        document.getElementById('alert').style.display = 'none';
-        console.log('hah')
-    }
-    data.forEach(datas => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML= `
+const displayPhoneResult = (data) => {
+  if (data.length == 0) {
+    document.getElementById("alert2").style.display = "block";
+  }
+  const searchResult = document.getElementById("search-result");
+  searchResult.innerHTML = "";
+
+  if (searchResult == "") {
+    document.getElementById("alert2").style.display = "none";
+    console.log("hah");
+  }
+  data.splice(0, 20).forEach((datas) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
         <div class="card text-center rounded-3">
             <img src="${datas.image}" class="card-img-top w-50 h-75 mx-auto pt-3" alt="...">
             <div class="card-body">
                 <h3 class="card-title">${datas.phone_name}</h3>
                 <h4>Brand: ${datas.brand}</h4>
-                <button onclick="loadDetails('${datas.slug}')" class="bg-primary text-white px-3 py-2 border-0 rounded-pill fw-bold" type="button">Explore now</button>
+                <a href="#phone-details"><button onclick="loadDetails('${datas.slug}')" class="bg-primary text-white px-3 py-2 border-0 rounded-pill fw-bold" type="button">Explore now</button></a>
             </div>
         </div>
         `;
-        searchResult.appendChild(div)
-    })
-}
+    searchResult.appendChild(div);
+  });
+};
 
 // getting phone id
-const loadDetails = phoneId => {
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-    fetch(url)
-    .then(res => res.json())
-    .then(main => displayDetails(main.data))
-}
+const loadDetails = (phoneId) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((main) => displayDetails(main.data));
+};
 
 // more details dynamically
-const displayDetails = phone => {
-    console.log(phone);
-    const phoneDetails = document.getElementById('phone-details');
-    phoneDetails.textContent = '';
-    const div = document.createElement('div')
-    div.classList.add('row');
-    div.innerHTML = `
+const displayDetails = (phone) => {
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("row");
+  div.innerHTML = `
     <div class="col-md-6 bg-white text-center py-3">
         <img src="${phone.image}" alt="#">
     </div>
     <div class="col-md-6 bg-white py-3">
         <h2>Phone Details</h2>
         <h4>Name: ${phone.name}</h4>
-        <h4>Released Date: ${phone.releaseDate}</h4>
+        <h4>Released Date: ${
+          phone?.releaseDate ? phone?.releaseDate : `No release date found`
+        }</h4>
         <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="headingOne">
@@ -82,10 +88,12 @@ const displayDetails = phone => {
                   </h2>
                   <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                      <p><b>Chipset: </b>${phone.mainFeatures.chipSet}</p> 
-                      <p><b>Display Size: </b>${phone.mainFeatures.displaySize}</p>
-                      <p><b>Memory: </b>${phone.mainFeatures.storage}</p>
-                      <p><b>Sensors: </b>${phone.mainFeatures.sensors}</p>
+                      <p><b>Chipset: </b>${phone?.mainFeatures?.chipSet}</p> 
+                      <p><b>Display Size: </b>${
+                        phone?.mainFeatures?.displaySize
+                      }</p>
+                      <p><b>Memory: </b>${phone?.mainFeatures?.storage}</p>
+                      <p><b>Sensors: </b>${phone?.mainFeatures?.sensors}</p>
                     </div>
                   </div>
                 </div>
@@ -97,12 +105,30 @@ const displayDetails = phone => {
                   </h2>
                   <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                    <p><b>Bluetooth: </b>${phone.others.Bluetooth}</p> 
-                    <p><b>GPS: </b>${phone.others.GPS}</p>
-                    <p><b>NFC: </b>${phone.others.NFC}</p>
-                    <p><b>Radio: </b>${phone.others.Radio}</p>
-                    <p><b>USB: </b>${phone.others.USB}</p>
-                    <p><b>WLAN: </b>${phone.others.WLAN}</p>
+                    <p><b>Bluetooth: </b>${
+                      phone?.others?.Bluetooth
+                        ? phone?.others?.Bluetooth
+                        : `no data found`
+                    }</p> 
+                    <p><b>GPS: </b>${
+                      phone?.others?.GPS ? phone?.others?.GPS : `no data found`
+                    }</p>
+                    <p><b>NFC: </b>${
+                      phone?.others?.NFC ? phone?.others?.NFC : `no data found`
+                    }</p>
+                    <p><b>Radio: </b>${
+                      phone?.others?.Radio
+                        ? phone?.others?.Radio
+                        : `no data found`
+                    }</p>
+                    <p><b>USB: </b>${
+                      phone?.others?.USB ? phone?.others?.USB : `no data found`
+                    }</p>
+                    <p><b>WLAN: </b>${
+                      phone?.others?.WLAN
+                        ? phone?.others?.WLAN
+                        : `no data found`
+                    }</p>
                     </div>
                   </div>
                 </div>
@@ -110,5 +136,5 @@ const displayDetails = phone => {
     </div>
     
     `;
-    phoneDetails.appendChild(div);
-}
+  phoneDetails.appendChild(div);
+};
